79afdf503130e8891ba58f9fc6d7eeba33875d10
scveloso <samtveloso@gmail.com>
2019-11-17 11:56:27 -0800

    Document TODOs

--- web-page-gen.rkt
+++ web-page-gen.rkt
@@ -35,7 +35,7 @@
 (define (extract-style-item style-str)
   (string-join (map (λ (s) s) (rest (string-split style-str " "))) " "))
 
-;; parse : listof sexp -> listof exp
+;; parse : listof sexp -> listof expression
 ;; Consumes a list of s-expressions (in our concrete "surface" syntax) and
 ;; generates the corresponding expressions.
 (define (parse lof-sexp)
@@ -67,8 +67,11 @@
                [_ (error 'parse "unable to parse ~a" style-str)])])
     (map helper styles-list)))
 
-;; interp : expression -> string
+;; interp : listof expression -> string
 ;; consumes an exp and returns a HTML component in a string
+;; TODO: For every expression we have, generate a random id and use that id in the HTML element to be
+;; generated and added, as well as the CSS element
+;; TODO: Add writing a CSS file with the CSS elements
 (define (interp the-exp)
   (type-case element (expr-elt the-exp)
     [create-paragraph (s) (string-append "<p>" (string-append s "</p>"))]))
@@ -95,6 +98,8 @@
 ;; Helper tests
 (test (extract-text-from-create-paragraph-call create-paragraph-test) "Lorem ipsum. ")
 (test (extract-style create-paragraph-test) "color red, font comic sans, size 12")
+(test (extract-style-item "color red") "red")
+(test (extract-style-item "font comic sans") "comic sans")
 
 ;; Parse tests
 (test (parse (list create-paragraph-test)) (list (expr
